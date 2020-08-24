695b54463018ece921db9ef96e2fcb4453312d65
happy-ghost00 <54895983+happy-ghost00@users.noreply.github.com>
2020-08-24 17:44:52 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
-using System.Security.Cryptography.X509Certificates;
-
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -20,23 +19,25 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        
+         
         Database1Entities database1Entities = new Database1Entities();
         private DialogResult uyar;
+
         private void Form1_Load(object sender, EventArgs e)
         {
             database1Entities.Database.Connection.ConnectionString = "data source=(LocalDB)\\MSSQLLocalDB;attachdbfilename=C:\\Users\\stajer\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\Database1.mdf;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
-            var liste = database1Entities.Tabledeneme.ToList();
+            var liste = database1Entities.Tabledenemes.ToList();
             dataGridView1.DataSource = liste;
         }
-
         void son()
         {
-            var liste = database1Entities.Tabledeneme.ToList();
+            var liste = database1Entities.Tabledenemes.ToList();
             dataGridView1.DataSource = liste;
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            var user = database1Entities.Tabledeneme.Where(x => x.username == textBox1.Text.Trim() && x.password == textBox2.Text.Trim() && x.age == textBox3.Text.Trim());
+            var user = database1Entities.Tabledenemes.Where(x => x.username == textBox1.Text.Trim() && x.password == textBox2.Text.Trim() && x.age == Convert.ToInt32(textBox3.Text.Trim()));
 
             if (user.Count() == 1 )
             {
@@ -47,10 +48,9 @@ namespace WindowsFormsApp1
                 MessageBox.Show("Yanlış Giriş","ERROR");
             }
         }
-
         private void button2_Click(object sender, EventArgs e)
         { 
-            if (database1Entities.Tabledeneme.Where(x=>x.username==textBox1.Text.Trim()).Any())
+            if (database1Entities.Tabledenemes.Where(x=>x.username==textBox1.Text.Trim()).Any())
             {
                 MessageBox.Show("Böyle Bir Kullanıcı Zaten Var!!", "ERROR");
             }
@@ -60,24 +60,24 @@ namespace WindowsFormsApp1
 
                 ROW.username = textBox1.Text.Trim();
                 ROW.password= textBox2.Text.Trim();
+                ROW.age = Convert.ToInt32(textBox3.Text.Trim());
                 
-                database1Entities.Tabledeneme.Add(ROW);
+                database1Entities.Tabledenemes.Add(ROW);
                 MessageBox.Show("Kaydediliyor");
 
                 database1Entities.SaveChanges();
                 son();
             }
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             uyar = MessageBox.Show(this, textBox1.Text + " Kullanıcısının Kaydını Silmek istiyor musunuz?", "SİLME UYARISI", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
             if (uyar == DialogResult.Yes)
             {
-                Tabledeneme a= database1Entities.Tabledeneme.Where(x => x.username == textBox1.Text.Trim()).FirstOrDefault();
+                Tabledeneme a= database1Entities.Tabledenemes.Where(x => x.username == textBox1.Text.Trim()).FirstOrDefault();
                 if (a != null)
                 {
-                    database1Entities.Tabledeneme.Remove(a);
+                    database1Entities.Tabledenemes.Remove(a);
                     database1Entities.SaveChanges();
                     son();
                 }
@@ -87,49 +87,49 @@ namespace WindowsFormsApp1
                 }
             }
             else
-            { 
-
-            };
+            { }
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
-            var guncelle_un = database1Entities.Tabledeneme.Find(passwordDataGridViewTextBoxColumn);
+            var guncelle_un = database1Entities.Tabledenemes.Find(passwordDataGridViewTextBoxColumn);
             guncelle_un.username = textBox1.Text;
             
             database1Entities.SaveChanges();
             son();
             MessageBox.Show("Bilgileriniz Başarıyla Güncellendi");
         }
-
         private void button5_Click(object sender, EventArgs e)
         {
-            var guncelle_pas = database1Entities.Tabledeneme.Find(usernameDataGridViewTextBoxColumn);
+            var guncelle_pas = database1Entities.Tabledenemes.Find(usernameDataGridViewTextBoxColumn);
             guncelle_pas.password = textBox2.Text;
             
             database1Entities.SaveChanges();
             son();
             MessageBox.Show("Şifreniz Başarıyla Güncellendi");
         }
-
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
-        }
-
+        {        }
         private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+        {        }
         private void button6_Click(object sender, EventArgs e)
         {
-            var guncelle_age = database1Entities.Tabledeneme.Where(x => x.age == textBox3.Text.Trim()).FirstOrDefault();
-            guncelle_age.age = Convert.ToString(textBox3.Text);
+            var guncelle_age = database1Entities.Tabledenemes.Where(w => w.username == textBox1.Text.Trim()).FirstOrDefault();
+            if (guncelle_age != null)
+            {
+                guncelle_age.age = Convert.ToInt32(textBox3.Text.Trim());
 
-            database1Entities.SaveChanges();
-            son();
-            MessageBox.Show("Yaşınız Başarıyla Değiştirildi");
+                database1Entities.SaveChanges();
+                son();
+                MessageBox.Show("Yaşınız Değiştirildi");
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+            var max_age = database1Entities.Tabledenemes.Max(w => w.age);
+            var a=database1Entities.Tabledenemes.Where(w => w.age == max_age).FirstOrDefault();
+            label5.Text = a.username;
         }
     }
 }
